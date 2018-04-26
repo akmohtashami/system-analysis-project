d064f1ced3803eb7481cf82a3d2f000fb6fe629b
Peyman <peyman.jabarzade@gmail.com>
2018-04-26 17:43:37 +0430

    update test register

--- code/tests/test_contact_admin.py
+++ code/tests/test_contact_admin.py
@@ -16,8 +16,7 @@ class ContactAdminTest(BaseTest):
         self.email = self.form.find_element_by_name('email')
         self.text = self.form.find_element_by_name('text')
         self.submit_button = self.form.find_element_by_name("submit")
-
-    def fillForm(self):
+        
         self.name.send_keys("Tester")
         self.email.send_keys("test@gmail.com")
         self.text.send_keys("This is a test")
@@ -27,14 +26,13 @@ class ContactAdminTest(BaseTest):
 
     def test_text_multi_line(self):
         self.findForm()
-        self.fillForm()
         old_len = len(self.text.text)
         self.text.send_keys(Keys.ENTER)
         self.text.send_keys("And the test shall continue")
         new_len = len(self.text.text)
         self.assertNotEqual(old_len, new_len, "Multi-line text must be allowed")
 
-    def submit_form(self):
+    def submitForm(self):
         # Not using self.form.submit deliberately
         self.submit_button.click()
 
@@ -50,55 +48,46 @@ class ContactAdminTest(BaseTest):
 
     def test_ok_send(self):
         self.findForm()
-        self.fillForm()
-        self.text.clear()
-        self.submit_form()
+        self.submitForm()
         self.form.find_element_by_class_name("success")
-
 
     def test_empty_text(self):
         self.findForm()
-        self.fillForm()
         self.text.clear()
-        self.submit_form()
+        self.submitForm()
         self.assertTrue("error" in self.text.get_attribute("class"))
 
     def test_empty_name(self):
         self.findForm()
-        self.fillForm()
         self.name.clear()
-        self.submit_form()
+        self.submitForm()
         self.assertTrue("error" in self.name.get_attribute("class"))
 
     def test_empty_email(self):
         self.findForm()
-        self.fillForm()
         self.email.clear()
-        self.submit_form()
+        self.submitForm()
         self.assertTrue("error" in self.email.get_attribute("class"))
 
     def test_invalid_email_no_at_sign(self):
         self.findForm()
-        self.fillForm()
         self.email.clear()
         self.email.send_keys("salam")
-        self.submit_form()
+        self.submitForm()
         self.assertTrue("error" in self.email.get_attribute("class"))
 
     def test_invalid_email_no_tld(self):
         self.findForm()
-        self.fillForm()
         self.email.clear()
         self.email.send_keys("salam@gmail")
-        self.submit_form()
+        self.submitForm()
         self.assertTrue("error" in self.email.get_attribute("class"))
 
     def test_invalid_email_no_address(self):
         self.findForm()
-        self.fillForm()
         self.email.clear()
         self.email.send_keys("@gmail.com")
-        self.submit_form()
+        self.submitForm()
         self.assertTrue("error" in self.email.get_attribute("class"))
 
 
--- code/tests/test_register.py
+++ code/tests/test_register.py
@@ -1,4 +1,4 @@
-from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import StaleElementReferenceException
 
 from tests.base import BaseTest
 
@@ -8,23 +8,81 @@ class RegisterTest(BaseTest):
         self.getURL('register')
         self.assertIn('Register', driver.title)
 
-    def test_register(self):
+    def fillForm(self):
         self.firstname = self.driver.find_element_by_name('firstname')
         self.lastname = self.driver.find_element_by_name('lastname')
         self.email = self.driver.find_element_by_name('email')
         self.password = self.driver.find_element_by_name('password')
         self.password_confirmation = self.driver.find_element_by_name('passwordConfirmation')
-        self.submit = self.driver.find_element_by_name('submit')
+        self.submit_button = self.driver.find_element_by_name('submit')
 
-        self.firstname.send_keys('peyman')
-        self.lastname.send_keys('jabarzade')
-        self.email.send_keys('peyman.jabarzade@gmail.com')
-        self.password.send_keys('password:D')
-        self.password_confirmation.send_keys('password:D')
+        self.firstname.send_keys('firstname')
+        self.lastname.send_keys('lastname')
+        self.email.send_keys('test@gmail.com')
+        self.password.send_keys('password')
+        self.password_confirmation.send_keys('password')
 
-        submit()
+    def submitForm(self):
+        # Not using self.form.submit deliberately
+        self.submit_button.click()
+
+        def form_has_gone_stale():
+            try:
+                self.form.find_element_by_name('firstname')
+                return False
+            except StaleElementReferenceException:
+                return True
+
+    def test_form_inputs(self):
+        self.fillForm()
+
+    def test_register(self):
+        self.fillForm()
+        self.submitForm()
         check_user_registered()
 
     def test_empty_firstname(self):
-        submit = self.driver.find_element_by_name('submit')
-        submit.click()
+        self.fillForm()
+        self.firstname.clear()
+        self.submitForm()
+        self.assertTrue("error" in self.firstname.get_attribute("class"))
+
+    def test_empty_lastname(self):
+        self.fillForm()
+        self.lastname.clear()
+        self.submitForm()
+        self.assertTrue("error" in self.lastname.get_attribute("class"))
+
+    def test_empty_email(self):
+        self.fillForm()
+        self.email.clear()
+        self.submitForm()
+        self.assertTrue("error" in self.email.get_attribute("class"))
+
+    def test_invalid_email_no_at_sign(self):
+        self.fillForm()
+        self.email.clear()
+        self.email.send_keys("test.com")
+        self.submitForm()
+        self.assertTrue("error" in self.email.get_attribute("class"))
+
+    def test_invalid_email_no_tld(self):
+        self.fillForm()
+        self.email.clear()
+        self.email.send_keys("test@gmail")
+        self.submitForm()
+        self.assertTrue("error" in self.email.get_attribute("class"))
+
+    def test_invalid_email_no_address(self):
+        self.fillForm()
+        self.email.clear()
+        self.email.send_keys("@gmail.com")
+        self.submitForm()
+        self.assertTrue("error" in self.email.get_attribute("class"))
+
+    def test_short_password(self):
+        self.fillForm()
+        self.password.clear()
+        self.password_confirmation.clear()
+        self.password.send_keys("passw")
+        self.password_confirmation.send_keys("passw")
