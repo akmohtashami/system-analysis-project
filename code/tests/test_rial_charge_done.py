3813f31e3e24c2ad0902b948fd8ca00ee1c51676
Amir Keivan Mohtashami <akmohtashami97@gmail.com>
2018-04-26 23:13:50 +0430

    Add rial charge tests

---
+++ code/tests/test_rial_charge_done.py
@@ -0,0 +1,23 @@
+from selenium.common.exceptions import StaleElementReferenceException
+
+
+from tests.amount_validate import AmountValidate
+from tests.base import BaseTest
+from tests.email_validate import EmailValidate
+from tests.utils import createCustomer
+
+
+class RialChargeRequestTest(BaseTest):
+
+    def setUp(self):
+        super(RialChargeRequestTest, self).setUp()
+        self.getURL("charge/done")
+
+    def test_result_shown(self):
+        self.driver.find_element_by_id("payment-result")
+
+
+
+
+
+
