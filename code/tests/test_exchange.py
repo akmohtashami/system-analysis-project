15a7605f082c562470dbe1fed9cbd467c4e5bd7a
Peyman <peyman.jabarzade@gmail.com>
2018-06-17 18:15:41 +0430

    changing test exchange

--- code/tests/test_exchange.py
+++ code/tests/test_exchange.py
@@ -5,23 +5,29 @@ from tests.base import BaseTest
 from tests.utils import createCustomer
 
 
-class ExchangeTest(BaseTest):
+class ExchangeTest(BaseTest, AmountValidate("output_amount")):
     def setUp(self):
         super(ExchangeTest, self).setUp()
-        createCustomer('test@gmail.com', 'password', 1000000, 0, 0)
+        createCustomer('test@gmail.com', 'password', 10000, 0, 0)
         self.login('test@gmail.com', 'password')
         self.getURL('exchange')
 
-    def findAndFillForm(self):
-        self.form = self.driver.find_element_by_css_selector("form[name='exchange_form']")
-        self.input_currency = Select(self.driver.find_element_by_name('inputCurrency'))
-        self.input_amount = self.driver.find_element_by_name('inputAmount')
-        self.output_currency = Select(self.driver.find_element_by_name('outputCurrency'))
-        self.output_amount = self.driver.find_element_by_name('outputAmount')
+    def findForm(self, form_name="exchange_form"):
+        self.form = self.driver.find_element_by_css_selector("form[name='"+form_name+"']")
+        self.input_currency = Select(self.driver.find_element_by_id('input_currency'))
+        self.input_amount = self.driver.find_element_by_id('input_amount')
+        self.output_currency = Select(self.driver.find_element_by_id('output_currency'))
+        self.output_amount = self.driver.find_element_by_id('output_amount')
         self.submit_button = self.driver.find_element_by_name('submit')
 
+    def fillForm(self):
         self.input_currency.select_by_value('IRR')
         self.output_currency.select_by_value('USD')
+        self.output_amount.send_keys('1')
+
+    def findAndFillForm(self):
+        self.findForm()
+        self.fillForm()
 
     def submitForm(self):
         # Not using self.form.submit deliberately
@@ -29,20 +35,19 @@ class ExchangeTest(BaseTest):
 
         def form_has_gone_stale(driver):
             try:
-                self.form.find_element_by_name('name')
+                self.form.find_element_by_name('submit')
                 return False
             except StaleElementReferenceException:
                 return True
 
         self.wait_for(form_has_gone_stale)
-        self.findAndFillForm()
+        self.findNewAmount()
 
     def test_form_inputs(self):
         self.findAndFillForm()
-        self.assertTrue(self.input_amount.text == '0' and self.output_amount.text == '0')
 
     def test_ok_exchange(self):
         self.findAndFillForm()
-        self.input_amount.send_keys('1000')
         received = int(self.output_amount.text)
         self.submitForm()
+        self.findForm("exchange_confirm_form")
