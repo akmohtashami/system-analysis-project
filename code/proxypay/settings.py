85e9aa28dfad44bed63cd263bd7ba76dd6ed4ef6
Amir Keivan Mohtashami <akmohtashami97@gmail.com>
2018-08-25 02:33:04 +0200

    Translate to persian

--- code/proxypay/settings.py
+++ code/proxypay/settings.py
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = "fa-ir"
+LANGUAGE_CODE = "fa"
 
 TIME_ZONE = 'UTC'
 
@@ -126,6 +126,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = [os.path.join(BASE_DIR, "locale")]
 
 
 # Static files (CSS, JavaScript, Images)
