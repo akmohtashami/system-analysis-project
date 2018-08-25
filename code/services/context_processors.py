ebf4909c9defcaa75665e9531f35855a5c1547c5
Amir Keivan Mohtashami <akmohtashami97@gmail.com>
2018-08-25 13:33:58 +0200

    Small improvements

--- code/services/context_processors.py
+++ code/services/context_processors.py
@@ -2,8 +2,7 @@ from services.models import ServiceType
 
 
 def available_services(request):
-
     return {
-        "available_services": ServiceType.objects.filter(is_active=True).exclude(short_name='withdraw').all(),
+        "available_services": ServiceType.objects.filter(is_active=True).exclude(short_name='withdraw').all() if request.user.is_authenticated and request.user.is_customer() else None,
         "withdraw_available": ServiceType.objects.filter(short_name='withdraw', is_active=True).exists()
     }
